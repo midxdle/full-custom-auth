970a27305cdfd54ddcc7c7d05c68e788601516ab
midxdle <sina.heydarian.sh@gmail.com>
2023-09-05 19:31:00 +0330

    create custom jason web token for auth by using custom hashing methods

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { CryptoModule } from './helpers/crypto/crypto.module';
+import { JwtModule } from './helpers/jwt/jwt.module';
 
 @Module({
+  imports: [CryptoModule, JwtModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
