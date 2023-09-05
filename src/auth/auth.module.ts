55c0dea2e4ff6aea0600eb7c3ed8f71835768109
midxdle <sina.heydarian.sh@gmail.com>
2023-09-05 19:35:35 +0330

    add jwt module config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { CryptoModule } from './helpers/crypto/crypto.module';
 import { JwtModule } from './helpers/jwt/jwt.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CryptoModule, JwtModule],
+  imports: [
+    JwtModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+    CryptoModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
